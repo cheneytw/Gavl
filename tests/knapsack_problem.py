a57e8b6916b10a2f74bb216705beb0b0add4561d
cheney_tw <cheneyliu.tw@gmail.com>
2024-05-08 22:31:54 +0800

    dd

--- tests/knapsack_problem.py
+++ tests/knapsack_problem.py
@@ -1,90 +1,55 @@
-"""
-In this file it is shown an example of the knapsack problem (the one explained in the README).
-"""
-import Gavl.Gavl as Gavl  # !!! This should be substituted by 'import Gavl'.
+# 匯入 Gavl 模組，這裡需要將 'import Gavl.Gavl as Gavl' 改為 'import Gavl'，假設適當的路徑已設定
+import Gavl.Gavl as Gavl
 
-ga = Gavl.Gavl()  # Initialize
+ga = Gavl.Gavl()  # 初始化遺傳演算法
 
-# Compulsory hyperparameters
+# 必要的超參數設定
+ga.set_hyperparameter('size_population', 50)  # 設定族群大小為 50
+ga.set_hyperparameter('min_length_chromosome', 1)  # 設定染色體最小長度為 1 個基因
+ga.set_hyperparameter('max_length_chromosome', 9)  # 設定染色體最大長度為 9 個基因（有 9 個物品可選）
 
-ga.set_hyperparameter('size_population', 50)  # Set the size of the population to 50 individuals
-ga.set_hyperparameter('min_length_chromosome', 1)  # Set the minimum length of the individual to 1 genes
-ga.set_hyperparameter('max_length_chromosome',
-                      9)  # Set the maximum length of the individual to 9 genes (there are 9 items)
+# 最大允許重量設定
+MAX_WEIGTH = 15
+# 超重每公斤的懲罰
+PENALIZATION = 10
 
-# Definition of the fitness:
-# The fitness function receives the chromosome, which is a list with the genes. It must return the fitness value (number).
-
-MAX_WEIGTH = 15  # Maximum allowed weight
-PENALIZATION = 10  # Penalization for each kg over MAX_WEIGTH
-
-# Dictionary ---> {'item': (price, weight)}
-prices_weights = {
-    'pen': (5, 3),
-    'pencil': (4, 2),
-    'food': (7, 6),
-    'rubber': (3, 1),
-    'book': (10, 9),
-    'scissors': (6, 3),
-    'glasses': (7, 5),
-    'case': (7, 7),
-    'sharpener': (2, 1)
-}
+# 物品的價格與重量，用字典表示，格式為 {'物品名稱': (價格, 重量)}
+prices_weights = {'pen': (5, 3), 'pencil': (4, 2), 'food': (7, 6), 'rubber': (3, 1), 'book': (10, 9), 'scissors': (6, 3), 'glasses': (7, 5), 'case': (7, 7), 'sharpener': (2, 1)}
 
 
 def fun_fitness(chromosome):
-    """ Definition of the fitness function.
-
-    :param chromosome: (list of genes) Chromosome of the individual that is being analyzed.
+    """ 定義適應度函數。
+    
+    :param chromosome: (基因列表) 正在分析的個體的染色體。
     """
-    fitness = 0  # Cumulative fitness
-    sum_weights = 0  # Cumulative weights
+    fitness = 0  # 累積的適應度
+    sum_weights = 0  # 累積的重量
     for item in chromosome:
-        fitness += prices_weights[item][0]
-        sum_weights += prices_weights[item][1]
-    fitness -= PENALIZATION * (max(sum_weights, MAX_WEIGTH) - MAX_WEIGTH)  # Add penalization
+        fitness += prices_weights[item][0]  # 增加價格到適應度
+        sum_weights += prices_weights[item][1]  # 累積重量
+    if sum_weights > MAX_WEIGTH:  # 如果超過最大允許重量，則減去懲罰
+        fitness -= PENALIZATION * (sum_weights - MAX_WEIGTH)
     return fitness
 
 
-ga.set_hyperparameter('fitness', fun_fitness)  # Set the fitness function
+# 設定適應度函數
+ga.set_hyperparameter('fitness', fun_fitness)
 
-# Definition of the values that the genes can take
-
+# 定義基因可能的值
 possible_genes = ['pen', 'pencil', 'food', 'rubber', 'book', 'scissors', 'glasses', 'case', 'sharpener']
-ga.set_hyperparameter('possible_genes',
-                      possible_genes)  # The possible values that the genes can take are the items' names
+ga.set_hyperparameter('possible_genes', possible_genes)  # 設定基因可能的值為物品名稱
 
-# # In case it is wanted to NOT allow chromosomes that overpass the maximum allowed weight it can be done the following (notice that it has preferred to define a penalization term instead):
-#
-# def check_valid_individual(chromosome):
-#     """ Definition of a valid individual.
-#
-#     :param chromosome: (list of genes) Chromosome of the individual that is being analyzed.
-#     """
-#     sum_weights = 0  # Cumulative weights
-#     for item in chromosome:
-#         sum_weights += prices_weights[item][1]
-#     if sum_weights > MAX_WEIGTH:
-#         return False
-#     else:
-#         return True
-#
-# ga.set_hyperparameter('check_valid_individual', check_valid_individual)
+# 設定其他參數
+ga.set_hyperparameter('termination_criteria', {'max_num_generation_reached': 30})  # 設定終止條件為達到 30 代
+ga.set_hyperparameter('repeated_genes_allowed', 0)  # 不允許重複基因
+ga.set_hyperparameter('minimize', 0)  # 設定目標是最大化適應度
+ga.set_hyperparameter('elitism_rate', 0.1)  # 設定精英比率為 0.1
+ga.set_hyperparameter('mutation_rate', 0.2)  # 設定突變率為 0.2
+ga.set_hyperparameter('mutation_type', 'both')  # 設定突變類型
+ga.set_hyperparameter('keep_diversity', 5)  # 每 5 代維持一次多樣性
+ga.set_hyperparameter('show_progress', 1)  # 顯示演算法進展
 
-# Set other parameters
+best_individual = ga.optimize()  # 優化求解
 
-ga.set_hyperparameter('termination_criteria', {
-    'max_num_generation_reached': 30})  # Set the termination criteria to maximum number of generations (in this case 30 generations).
-ga.set_hyperparameter('repeated_genes_allowed',
-                      0)  # In this case the repeated genes are NOT allowed ---> Each item can be taken only once
-ga.set_hyperparameter('minimize', 0)  # In this case the goal is to maximize the fitness
-ga.set_hyperparameter('elitism_rate', 0.1)  # Set an elitism rate of 0.1
-ga.set_hyperparameter('mutation_rate', 0.2)  # Set a mutation rate of 0.2
-ga.set_hyperparameter('mutation_type', 'both')  # Set the mutation type
-ga.set_hyperparameter('keep_diversity', 5)  # Set a mechanism to keep the diversity every 5 generations
-ga.set_hyperparameter('show_progress',
-                      1)  # Show progress of the genetic algorithm (this will print the generation number)
-
-best_individual = ga.optimize()  # Optimize
-
-best_individual, population, historic_fitness = ga.get_results()  # Get other results
+# 獲取結果
+best_individual, population, historic_fitness = ga.get_results()
